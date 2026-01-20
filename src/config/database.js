36026debba95274397068a8a0dd31d882adf05c9
Saurav Kumar <sauravsaran99@gmail.com>
2026-01-20 14:43:24 +0530

    deployment database config

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,21 @@
-const { Sequelize } = require('sequelize');
-require('dotenv').config();
+// const { Sequelize } = require("sequelize");
+// require("dotenv").config();
+
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: process.env.DB_HOST,
+//     dialect: process.env.DB_DIALECT,
+//     logging: false,
+//   },
+// );
+
+// module.exports = sequelize;
+
+const { Sequelize } = require("sequelize");
+require("dotenv").config();
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
@@ -7,9 +23,23 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
+    port: process.env.DB_PORT,
+    dialect: "mysql",
     logging: false,
-  }
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 60000,
+      idle: 10000,
+    },
+    dialectOptions: {
+      connectTimeout: 60000,
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  },
 );
 
 module.exports = sequelize;
