5432167752fc0dd6c8b52ad60d3a5303a0103d68
Saurav Kumar <sauravsaran99@gmail.com>
2026-01-21 11:33:49 +0530

    minor databaes config change

--- src/config/database.js
+++ src/config/database.js
@@ -1,3 +1,19 @@
+// const { Sequelize } = require("sequelize");
+// require("dotenv").config();
+
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: process.env.DB_HOST,
+//     dialect: process.env.DB_DIALECT,
+//     logging: false,
+//   },
+// );
+
+// module.exports = sequelize;
+
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
@@ -7,8 +23,15 @@ const sequelize = new Sequelize(
   process.env.DB_PASSWORD,
   {
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
+    port: process.env.DB_PORT,
+    dialect: "mysql",
     logging: false,
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
   },
 );
 
